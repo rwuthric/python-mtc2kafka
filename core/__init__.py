d46c36c613df3ac0dec4d043fd01e35e02fcdd09
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2022-08-21 01:55:14 -0400

    Created explicite MTConnect deserializers besides Mixin

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,4 @@
 from .serializers import MTCSerializersMixin
-from .deserializers import MTCDeserializersMixin
+from .deserializers import mtc_key_deserializer, mtc_value_deserializer, MTCDeserializersMixin
 from .mtcdocument import MTCDocumentMixing
 from .exceptions import ImproperlyConfigured
--- core/deserializers.py
+++ core/deserializers.py
@@ -1,12 +1,29 @@
 import ast
 
-class MTCDeserializersMixin():
-    """ A mixin for Kafka MTConnect DataItems deserializers """
-    
-    def mtc_key_deserializer(self, key):
+"""
+Kafka deserializers for MTConnect DataItems
+"""
+
+def mtc_key_deserializer(key):
         """ Kafka key deserializer for MTConnect DataItems """
         return key.decode('utf-8')
+    
+def mtc_value_deserializer(value):
+        """ Kafka value serializer for MTConnect DataItems """
+        return ast.literal_eval(value.decode('utf-8'))
+
+
+
+"""
+Mixin for Kafka deserializers of MTConnect DataItems
+"""
+
+class MTCDeserializersMixin():
+
+    def mtc_key_deserializer(self, key):
+        """ Kafka key deserializer for MTConnect DataItems """
+        return mtc_key_deserializer(key)
 
     def mtc_value_deserializer(self, value):
         """ Kafka value serializer for MTConnect DataItems """
-        return ast.literal_eval(value.decode('utf-8'))
+        return mtc_value_deserializer(value)
