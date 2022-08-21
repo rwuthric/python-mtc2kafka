ddcec7595feb552f2d10237583938d5a97a0324a
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2022-08-21 02:05:31 -0400

    Initial version

--- core/__init__.py
+++ core/__init__.py
@@ -2,3 +2,4 @@ from .serializers import MTCSerializersMixin
 from .deserializers import mtc_key_deserializer, mtc_value_deserializer, MTCDeserializersMixin
 from .mtcdocument import MTCDocumentMixing
 from .exceptions import ImproperlyConfigured
+from .mtckafkaconsumer import MTCKafkaConsumer
---
+++ core/mtckafkaconsumer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+from mtc2kafka.core import MTCDeserializersMixin
+
+
+class MTCKafkaConsumer(KafkaConsumer, MTCDeserializersMixin):
+    """ KafkaConsumer with MTConnect deserializer """
+       
+    def __init__(self, *topic, **configs):
+        """ Constructor """
+        super(MTCKafkaConsumer, self).__init__(*topic, **configs,
+                                               key_deserializer=self.mtc_key_deserializer,
+                                               value_deserializer=self.mtc_value_deserializer)
+    
