78e2144749c35ff2e9eefb2d086eedea3e23e16f
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2022-11-06 14:55:18 -0500

    Improved code documentation

--- connectors/csvsinkconnector.py
+++ connectors/csvsinkconnector.py
@@ -8,23 +8,26 @@ from mtc2kafka.core import MTCDeserializersMixin, ImproperlyConfigured
 class CSVSinkConnector(MTCDeserializersMixin):
     """
     Kafka sink connector to CSV files
-    Saves Kafka messages with a key in 'keys' to CSV files in 'storageFolder'
+    Saves Kafka messages containing MTConnect DataItems
+    with a key in 'keys' to CSV files in 'storageFolder'
 
     Children have to define the following attributes:
 
      bootstrap_servers = ['kafka_server1:9092']   # List of Kafka bootstrap servers
      topic = 'my_topic'                           # Kafka topic to read from
-     keys = ['key1' , 'key2']                     # List of keys that will be saved
-     meta_data = ['key3', 'key4']                 # List of keys that will be saved as meta data
-     storageFolder = '/path/to/storage/folder'    # Full path where the data should be saved to
+     keys = ['key1' , 'key2']                     # List of keys of MTConnect DataItems that will be saved
+     meta_data = ['key3', 'key4']                 # List of keys MTConnect DataItems that will be saved as meta data
+     storageFolder = '/path/to/storage/folder'    # Full path where the data will be saved to
      start_key = 'start'                          # Key that will trigger start of saving
+                                                  # Its value is used to create a sub-folder 
+                                                  # within 'storageFolder'
      stop_key = 'stop'                            # Key that will trigger stop of saving
 
 
     Children can optionnaly define the following attributes:
 
      history_file = 'name_of_file'                # Full name (including path) of file for history data
-                                                  # If none is defined will assign one based on class name in current path
+                                                  # If none is defined will assign one in current path based on class name
     """
 
     # Attributes to be defined by children
@@ -62,21 +65,29 @@ class CSVSinkConnector(MTCDeserializersMixin):
         print(str(self.current))
 
     def store_to_single_file(self, mode='w'):
-        """ Stores DataItems with a key in keys to a single file in storageFolder """       
+        """
+        Stores DataItems with a key in keys to a single file in storageFolder
+        If mode='w' (default) an new file is created
+        If mode='a' data are appended to existing file
+        """
         self.store(self.SAVE_MODE_SINGLE_FILE, mode)
-    
+
     def store_to_multiple_files(self, mode='w'):
-        """ Stores DataItems with a key in keys to mutiple files in storageFolder """           
+        """ 
+        Stores DataItems with a key in keys to mutiple files in storageFolder
+        If mode='w' (default) an new file is created for each key
+        If mode='a' data are appended to existing files
+        """
         self.store(self.SAVE_MODE_MULTIPLE_FILES, mode)
 
-    def save_current(self):
-        """ Saves current to history_file """
-        f = open(self.history_file, 'w')
-        f.write(str(self.current))
-        f.close()
-        
     def load_current(self):
-        """ Loads current from history_file """
+        """
+        Loads 'current' from history_file
+        'current' contains the state of the MTConnect device in form of:
+         - the current values of all keys in 'keys'
+         - the kafka_offset of the latest message read
+         - the timestamp of this state
+        """
         self.current = {}
         if os.path.isfile(self.history_file):
             f = open(self.history_file, 'r')
@@ -92,8 +103,18 @@ class CSVSinkConnector(MTCDeserializersMixin):
             self.current['timestamp'] = 'UNAVAILABLE'
         self.previous = self.current.copy()
 
+    def save_current(self):
+        """ Saves 'current' to history_file """
+        f = open(self.history_file, 'w')
+        f.write(str(self.current))
+        f.close()
+
     def get_storage_folder(self):
-        """ Returns folder where data will be stored and creates it if needed """
+        """ 
+        Returns folder where data will be stored and creates it if needed
+        The recieved value of the 'start_key' is used to create
+        a sub-folder within 'storageFolder'
+        """
         folder = os.path.join(self.storageFolder, self.current[self.start_key])
         Path(folder).mkdir(parents=True, exist_ok=True)
         return folder
@@ -119,7 +140,7 @@ class CSVSinkConnector(MTCDeserializersMixin):
                     self.files[key].write("%s,%s\n" % ("timestamp", key))
                 self.files[key].write("%s,%s\n" % (self.current['timestamp'], self.current[key]))
                 self.files[key].flush()
-                    
+
     def close_save_files(self, save_mode):
         """ Closes files for saving """
         if self.files is None:
@@ -144,7 +165,7 @@ class CSVSinkConnector(MTCDeserializersMixin):
         f.close()
 
     def store(self, save_mode, mode='w'):
-        """ Stores DataItems with a key in keys to the storageFolder """
+        """ Stores DataItems with their key in keys to the storageFolder """
         print("Storing dataItems " + str(self.keys) + " from topic " + self.topic)
         print("Destination : " + self.storageFolder)
 
@@ -158,13 +179,13 @@ class CSVSinkConnector(MTCDeserializersMixin):
         if 'kafka_offset' in self.current:
             consumer.topics()
             consumer.poll()
-            consumer.seek(part, self.current['kafka_offset']+1)
+            consumer.seek(part, self.current['kafka_offset'] + 1)
 
         record = False
         for message in consumer:
             print("%s\t%s=%s" % (message.headers, message.key, message.value['value']))
 
-            # keeps history of meta_data and data
+            # copy 'current' to 'previous', updates current state and saves it
             if message.key in self.meta_data or message.key in self.keys:
                 self.previous = self.current.copy()
                 self.current[message.key] = message.value['value']
