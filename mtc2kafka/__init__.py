adc97ef13cb8fe371699d6da4042e8ff2d6b5695
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2023-04-02 23:03:16 -0400

    Improved package metadata

--- mtc2kafka/__init__.py
+++ mtc2kafka/__init__.py
@@ -6,4 +6,7 @@ Python library to stream MTConnect data to Kafka
 
 __version__ = "0.1.0"
 __author__ = 'Rolf Wuthrich'
+__email__ = 'rolf.wuthrich@concordia.ca'
+__maintainer__ = 'Rolf Wuthrich'
 __credits__ = 'Concordia University'
+__status__ = 'Development'
--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 # mtc2kafka package build script
 
 from setuptools import setup, find_packages
+import mtc2kafka
 
 setup(
     name='mtc2kafka',
-    version='0.1.0',    
+    version=mtc2kafka.__version__,    
     description='Python library to stream MTConnect data to Apache Kafka',
     url='https://github.com/rwuthric/mtc2kafka',
-    author='Rolf Wuthrich',
-    author_email='rolf.wuthrich@concordia.ca',
+    author=mtc2kafka.__author__,
+    author_email=mtc2kafka.__email__,
     license='BSD 3-Clause License',
     packages=find_packages(),
     install_requires=['requests', 'kafka-python'],
